cb1b6960ef79625d41c59527223bf29f31f596ce
pappat <36902828+pappat@users.noreply.github.com>
2021-02-18 21:19:05 +0100

    elsoToltes

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <script src="jquery.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container border">
+        <div class="row">
+            <div class="col border text-center bg-success text-white m-1 p-2"><a href="">Főoldal</a> </div>
+            <div class="col text-center bg-success text-white m-1 p-2"><a href="">Hegycsucsok</a> </div>
+            <div class="col text-center bg-success text-white m-1 p-2"><a href="">Mászók</a> </div>
+            <div class="col text-center bg-success text-white m-1 p-2"><a href="">Újmászók</a> </div>
+        </div>
+    
+    
+    </div>
+    
+</body>
+</html>
